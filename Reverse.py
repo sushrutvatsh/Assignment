f322d91c18dd1f00e6a44e3d68f164ced1f0a596
sushrutvatsh <93922875+sushrutvatsh@users.noreply.github.com>
2021-11-10 12:31:16 +0530

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+word = input("Input a word to reverse: ")
+for char in range(len(word) - 1, -1, -1):
+print(word[char], end="")
+print("")
+
+
+# In[ ]:
+
+
+
+
